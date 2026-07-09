2bf92f8615d2a1750510bbe898fb0ac29422271b
Josué Torres <josuejesusts@gmail.com>
2026-07-09 14:03:04 -0500

    feat: implement YapeNotificationTask to handle Yape payment notifications in headless mode

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import { registerRootComponent } from "expo";
 import { AppRegistry } from "react-native";
-
 import App from "./App";
+import { parsearYRegistrarPagoYape } from "./src/hooks/useYapeListener";
+
+const YapeNotificationTask = async (notification) => {
+  try {
+    await parsearYRegistrarPagoYape(notification);
+  } catch (e) {
+    console.error("[HeadlessJS] Error:", e.message);
+  }
+};
+
 AppRegistry.registerHeadlessTask(
   "RNAndroidNotificationListenerHeadlessJsTaskService",
   () => YapeNotificationTask,
 );
 
-registerRootComponent(App);
+AppRegistry.registerComponent("main", () => App);
